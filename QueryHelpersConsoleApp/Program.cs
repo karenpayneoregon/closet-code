1903d8a24f6b4f73df692dcd776e6b9afd5d7043
Karen Payne <paynekaren@comcast.net>
2022-05-20 16:39:40 -0700

    queryhelpers

---
+++ QueryHelpersConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace QueryHelpersConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueryHelperDemo();
+            AlreadyCompleted();
+
+            Console.ReadLine();
+
+        }
+        private static void QueryHelperDemo()
+        {
+            Console.WriteLine(nameof(QueryHelperDemo));
+
+            Dictionary<string, string> queryArguments = new()
+            {
+                { "versionCode", "RTUK8L1" },
+                { "language", "2" }
+            };
+
+            var results = QueryHelpers.AddQueryString(
+                "api/GetCharacteristicsBulk", 
+                queryArguments);
+
+            Console.WriteLine(results);
+        }
+        private static void AlreadyCompleted()
+        {
+            Console.WriteLine(nameof(AlreadyCompleted));
+
+            string language = "S";
+
+            Dictionary<string, string> queryArguments = new()
+            {
+                { "transId", "64440952" },
+                { "lang", "E" }
+            };
+
+            if (language == "S")
+            {
+                queryArguments["lang"] = language;
+            }
+
+
+            var results = QueryHelpers.AddQueryString(
+                "advisoryNotice.kp", 
+                queryArguments);
+
+            Console.WriteLine(results);
+            Console.WriteLine("");
+
+
+        }
+
+    }
+}
---
+++ QueryHelpersConsoleApp/readme.md
@@ -0,0 +1,14 @@
+﻿# About
+
+Basic samples for [QueryHelpers.AddQueryString](https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.webutilities.queryhelpers.addquerystring?view=aspnetcore-6.0)
+
+## Packages
+
+```xml
+<ItemGroup>
+   <PackageReference Include="Microsoft.AspNetCore.Components" Version="5.0.9" />
+   <PackageReference Include="Microsoft.AspNetCore.Http" Version="2.2.2" />
+   <PackageReference Include="Microsoft.AspNetCore.Http.Extensions" Version="2.2.0" />
+   <PackageReference Include="Microsoft.AspNetCore.WebUtilities" Version="2.2.0" />
+</ItemGroup>
+```
