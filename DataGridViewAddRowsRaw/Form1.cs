48558fdb97fbef404822db5561ae421b3cb12443
Karen Payne <paynekaren@comcast.net>
2022-05-22 05:49:34 -0700

    forum question response for stackoverflow

---
+++ DataGridViewAddRowsRaw/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.ColumnCount = 4;
+
+            for (int index = 0; index < dataGridView1.Columns.Count; index++)
+            {
+                dataGridView1.Columns[index].HeaderText = $"Column {index +1}";
+            }
+
+            dataGridView1.Rows.Add(1, 2, 3, 4);
+
+        }
+
+        private void AddRowsButton_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView1.Rows.Count == 1)
+            {
+                dataGridView1.Rows.Add(1, 2, 3, 4);
+            }
+
+            for (int index = 0; index < 4; index++)
+            {
+                var row = dataGridView1.Rows.Cast<DataGridViewRow>()
+                    .LastOrDefault(gridRow => !gridRow.IsNewRow);
+
+                if (int.TryParse(row.Cells[3].Value.ToString(), out var lastValue))
+                {
+                    lastValue  += 1;
+
+                    dataGridView1.Rows.Add(
+                        lastValue,
+                        lastValue += 1,
+                        lastValue += 1,
+                        lastValue += 1);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ DataGridViewAddRowsRaw/readme.md
@@ -0,0 +1,10 @@
+﻿# About
+Stactoverflow question
+
+[C# Datagridview method Rows.Add works incorrectly - it add line on previous, not on last](https://stackoverflow.com/questions/72337220/c-sharp-datagridview-method-rows-add-works-incorrectly-it-add-line-on-previous)
+
+OP sets RowCount and ColumnCount which when adding a new row added it one up from the last row. Don't know why but my advice is not to set RowCount as presented in this project.
+
+The correct way would be to have a `DataSource` for the `DataGridView` and if a DataTable, simple add a new row or for a list add a new item which should also use a BindingList/BindingSource and if editing implement `INotifyPropertyChanged`.
+
+![Figure1](figure1.png)
