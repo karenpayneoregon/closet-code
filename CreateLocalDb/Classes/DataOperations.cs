3675e4c759381035ce173c4d2b3ba9639b0f2dc6
Karen Payne <paynekaren@comcast.net>
2022-06-04 06:52:28 -0700

    stubs

--- CreateLocalDb/Classes/DataOperations.cs
+++ CreateLocalDb/Classes/DataOperations.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 
 namespace CreateLocalDb.Classes
@@ -23,30 +19,56 @@ namespace CreateLocalDb.Classes
         }
         public static void Create()
         {
+            
             using var cn = new SqlConnection(MasterConnectionString());
             using var cmd = new SqlCommand() { Connection = cn };
+
             cmd.CommandText = @$"CREATE DATABASE[TRIAL] ON PRIMARY (NAME=TRIAL_A, FILENAME='{DataPath}\TRIAL_A.mdf)')
                                            LOG ON(NAME=TRIAL_log, FILENAME='{DataPath}\TRIAL_A.ldf')";
 
             try
             {
+                
                 cn.Open();
                 cmd.ExecuteNonQuery();
-                Console.WriteLine("Created");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
 
+                Console.WriteLine("Created");
+
+            }
+            catch (Exception localException)
+            {
+                Console.WriteLine(localException);
             }
         }
 
+        /// <summary>
+        /// Open connection
+        /// </summary>
         public static void Open()
         {
+
             using var cn = new SqlConnection(ConnectionString());
+
             cn.Open();
             Console.WriteLine("Open");
-            
+
+           
+        }
+
+        /// <summary>
+        /// Create tables and optionally populate
+        /// </summary>
+        public static void CreateTables()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Run select statements
+        /// </summary>
+        public static void ReadData()
+        {
+            throw new NotImplementedException();
         }
     }
 }
