4347b00eb4cb564caec0283579dbbaccead686a6
Karen Payne <paynekaren@comcast.net>
2022-06-26 08:28:24 -0700

    documentation

--- CommonLibrary/Classes/EnumerableExtensions.cs
+++ CommonLibrary/Classes/EnumerableExtensions.cs
@@ -6,9 +6,18 @@ namespace CommonLibrary.Classes
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Get random list of <see cref="T"/>
+        /// </summary>
+        /// <typeparam name="T">type</typeparam>
+        /// <param name="source">valid source of <see cref="T"/></param>
+        /// <returns>shuffled list</returns>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
             => source.Shuffle(new Random());
 
+        /// <summary>
+        /// Assertions
+        /// </summary>
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random random)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
@@ -17,6 +26,9 @@ namespace CommonLibrary.Classes
             return source.ShuffleIterator(random);
         }
 
+        /// <summary>
+        /// Get list
+        /// </summary>
         private static IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> source, Random rng)
         {
             List<T> buffer = source.ToList();
