bbbc74fc422fbd0014b37492a97c1071fd801fd9
Karen Payne <paynekaren@comcast.net>
2022-07-15 08:39:47 -0700

    new codes samples

---
+++ AskConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using AskConsoleApp2.Classes;
+using AskConsoleApp2.Models;
+using Spectre.Console;
+
+namespace AskConsoleApp2
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            UserChoices userChoices = Configuration.Get();
+
+            var table = new Table()
+                .RoundedBorder()
+                .AddColumn("[b]Setting[/]")
+                .AddColumn("[b]Value[/]")
+                .Alignment(Justify.Center)
+                .BorderColor(Color.LightSlateGrey)
+                .Title("[LightGreen]Choices[/]");
+
+
+            table.AddRow("PasswordLength", userChoices.PasswordLength.ToString());
+            table.AddRow("UseNumbers", userChoices.UseNumbers.ToYesNo());
+            table.AddRow("UseOtherSigns", userChoices.UseOtherSigns.ToYesNo());
+            table.AddRow("UseUppercaseLetters", userChoices.UseUppercaseLetters.ToYesNo());
+
+            AnsiConsole.Clear();
+            AnsiConsole.Write(table);
+
+            AnsiConsole.MarkupLine("[LightGreen]Press a key to close[/]");
+            Console.ReadLine();
+        }
+
+        [ModuleInitializer]
+        public static void Init()
+        {
+            Console.Title = "Code sample";
+        }
+    }
+}
