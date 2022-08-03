c2e66b36672a6aa350f23f6e5c4506b6be4fcdae
Karen Payne <paynekaren@comcast.net>
2022-08-03 02:38:59 -0700

    new code sample for events

--- AppSettingsConsole1/readme.md
+++ AppSettingsConsole1/readme.md
@@ -1,5 +1,5 @@
 ﻿# About
 
-Forum question [reading arrays of objects from appsettings.json](https://docs.microsoft.com/en-us/answers/questions/946022/reading-arrays-of-objects-from-appsettingsjson.html?childToView=946198#answer-946198)
+Forum question
 
 [How to read JSON as .NET objects (deserialize)](https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-5-0#how-to-read-json-as-net-objects-deserialize)
---
+++ EventsGetInvocationListFormsApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+using EventsGetInvocationListFormsApp.Extensions;
+
+namespace EventsGetInvocationListFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void SubscribeButton_Click(object sender, EventArgs e)
+        {
+            button1.Click += Button1OnClick;
+            button1.Click += Button1OnClick;
+            button1.Click += Button1OnClick;
+
+            UpdateCount();
+        }
+
+        private void Button1OnClick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(nameof(Button1OnClick));
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+
+        private void UnsubscribeButton_Click(object sender, EventArgs e)
+        {
+            button1.DisableEvents("Click");
+            UpdateCount();
+        }
+
+        private void EventCountButton_Click(object sender, EventArgs e)
+        {
+            UpdateCount();
+        }
+
+        private void UpdateCount()
+        {
+            listBox1.Items.Add(button1.EventCount("Click").ToString());
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+    }
+}
