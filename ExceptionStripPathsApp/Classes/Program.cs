051756dadb71ae4ef5c15efd3c3c6c7529254862
Karen Payne <paynekaren@comcast.net>
2022-08-05 07:42:31 -0700

    new code

---
+++ ExceptionStripPathsApp/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using W = ConsoleHelperLibrary.Classes.WindowUtility;
+
+// ReSharper disable once CheckNamespace
+namespace ExceptionStripPathsApp
+{
+    partial class Program
+    {
+        [ModuleInitializer]
+        public static void Init()
+        {
+            Console.Title = "Code sample";
+            W.SetConsoleWindowPosition(W.AnchorWindow.Center);
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+        }
+        static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine(((Exception)e.ExceptionObject).ToString());
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
+    }
+}
+
+
+
+
+
---
+++ ExceptionStripPathsApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExceptionStripPathsApp
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToDateTime(@"Ooops");
+            
+        }
+    }
+}
