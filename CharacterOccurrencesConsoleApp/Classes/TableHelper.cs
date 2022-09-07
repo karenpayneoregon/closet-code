87c840b314df0ddc30a13390f36de71e3a169c22
Karen Payne <paynekaren@comcast.net>
2022-09-07 06:09:23 -0700

    new sample, removed several samples

---
+++ CharacterOccurrencesConsoleApp/Classes/TableHelper.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CharacterOccurrencesConsoleApp.Classes;
+
+internal class TableHelper
+{
+    /// <summary>
+    /// Create table to display character results
+    /// </summary>
+    public static Table CreateViewTable()
+    {
+        return new Table()
+            .Border(TableBorder.Square)
+            .BorderColor(Color.DarkViolet)
+            .Centered()
+            .Title(ColorHelpers.Paint("Results"))
+            .AddColumn(new TableColumn("[b]Char[/]"))
+            .AddColumn(new TableColumn("[b]Count[/]"));
+    }
+}
--- SequenceIssues/readme.md
+++
@@ -1,3 +0,0 @@
-﻿# About
-
-Examples to determine if a sequence is broken and to find missing elements in a sequence. Code will be added over time.
