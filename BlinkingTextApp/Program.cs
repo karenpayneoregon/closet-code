0a583b8f45276b669d1eaf75966bc888e7cb12a0
Karen Payne <paynekaren@comcast.net>
2022-10-27 06:12:28 -0700

    new projects, mods on console web project

---
+++ BlinkingTextApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BlinkingTextApp
+{
+    internal partial class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            foreach (ConsoleColor c in Enum.GetValues(typeof(ConsoleColor)))
+            {
+                Console.ForegroundColor = c;
+                Console.WriteLine("!Hello World!");
+                await Task.Delay(100);
+                Console.Clear();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DateTimeApp/Classes/Operations.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace DateTimeApp.Classes;
+
+public static class Operations
+{
+    public static void NoDefaultToCurrentDateTime()
+    {
+        var cultureInfo = new CultureInfo("en");
+        string dateString = "26 November 2022";
+        var dateTime = DateTime.Parse(dateString, cultureInfo, DateTimeStyles.NoCurrentDateDefault);
+        AnsiConsole.MarkupLine($"[cyan]{dateTime}[/]");
+    }
+
+    public static void Example1()
+    {
+        
+        string[] format = { "dd-MM-yyyy HH:mm:ss" };
+        
+        string value = "26-10-2022 15:30:20";
+
+        //Check if string is a valid date by converting using DateTime.TryParseExact
+        if (DateTime.TryParseExact(value, format, CultureInfo.InvariantCulture, DateTimeStyles.NoCurrentDateDefault, out var datetime))
+        {
+            Console.WriteLine("Valid  Date: " + datetime);
+        }
+        else
+        {
+            Console.WriteLine("Invalid Date");
+        }
+    }
+}
