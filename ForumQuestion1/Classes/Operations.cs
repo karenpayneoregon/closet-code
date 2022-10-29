471cc3439a6ae1c7e14b116fafad4f0dde7c31d9
Karen Payne <paynekaren@comcast.net>
2022-10-29 16:15:44 -0700

    :trident: namespace

--- ForumQuestion1/Classes/Operations.cs
+++ ForumQuestion1/Classes/Operations.cs
@@ -1,33 +1,31 @@
-﻿using System.Configuration;
-using System.Text.Json;
+﻿using System.Text.Json;
 using ForumQuestion1.Models;
 
-namespace ForumQuestion1.Classes
+namespace ForumQuestion1.Classes;
+
+internal class Operations
 {
-    internal class Operations
+    private static string FileName => "data.json";
+    public static List<DataContainer> List { get; set; }
+    public static List<DataContainer> ReadFile()
     {
-        private static string FileName => "data.json";
-        public static List<DataContainer> List { get; set; }
-        public static List<DataContainer> ReadFile()
-        {
-            List = File.Exists(FileName) ? 
-                JsonSerializer.Deserialize<List<DataContainer>>(File.ReadAllText(FileName)) : 
-                new List<DataContainer>();
+        List = File.Exists(FileName) ? 
+            JsonSerializer.Deserialize<List<DataContainer>>(File.ReadAllText(FileName)) : 
+            new List<DataContainer>();
 
-            return List;
-        }
+        return List;
+    }
 
-        public static void Save()
-        {
-            if (List.Count <= 0) return;
-            string jsonString = JsonSerializer.Serialize(List, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(FileName, jsonString);
-        }
+    public static void Save()
+    {
+        if (List.Count <= 0) return;
+        string jsonString = JsonSerializer.Serialize(List, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(FileName, jsonString);
+    }
 
-        public static void Add(DataContainer sender)
-        {
-            var code = List.Last().TrackingCode;
-            sender.TrackingCode = Helpers.NextValue(code);
-        }
+    public static void Add(DataContainer sender)
+    {
+        var code = List.Last().TrackingCode;
+        sender.TrackingCode = Helpers.NextValue(code);
     }
 }
