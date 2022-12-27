b260357f110baf1821e626e611dade8f87cc5ad2
Karen Payne <paynekaren@comcast.net>
2022-12-27 03:46:02 -0800

    transfer

---
+++ EnumComparisionApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Drawing;
+
+namespace EnumComparisionApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+
+Console.WriteLine(
+    string.Join(",", 
+        Enumerable.Range(0, 10)
+            .Select(_ => Random.Shared.Next(150)
+            )));
+
+
+
+
+        Console.ReadLine();
+    }
+
+    private static void Sample1()
+    {
+        Console.WriteLine(Enum.TryParse("DairyProducts", true, out Categories category)
+            ? $"Found: {category}"
+            : $"Could not find the specified {nameof(Categories)}");
+
+        Console.WriteLine(Enum.TryParse("dairyproducts", true, out Categories categoryCaseInsensitive)
+            ? $"Found: {categoryCaseInsensitive}"
+            : $"Could not find the specified {nameof(Categories)}");
+
+        Console.WriteLine(Enum.TryParse("dairy products", out Categories categoryNotFound)
+            ? $"Found: {categoryNotFound}"
+            : $"Could not find the specified {nameof(Categories)}");
+    }
+}
+public enum Categories
+{
+    Beverages,
+    DairyProducts,
+    GrainsCereals,
+    Produce
+}
+
+class Circle
+{
+    public Point Center { get; private set; }
+    public Color Color { get; private set; }
+    public int Radius { get; private set; }
+    public void Draw()
+    {
+        // draw...
+    }
+}
---
+++ EnumComparisionApp/readme.md
@@ -0,0 +1,6 @@
+﻿# About
+
+Shows `Enum.TryParse` case insensitive.
+
+
+![Screen Shot](screenShot.png)
